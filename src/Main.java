7a5dd44d84e61c3ce06389d3a9b95287eb2085cf
Pavel <Pahenmurman@gmail.com>
2025-03-22 23:46:30 +0300

    hw14

--- src/Main.java
+++ src/Main.java
@@ -3,68 +3,42 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         System.out.println("task 1"); //  task1 ***************************************
-        byte[] three = new byte[3];
-        three[0] = 1;
-        three[1] = 2;
-        three[2] = 3;
-        System.out.println(Arrays.toString(three));
-        System.out.println();
-        float[] three2 = {1.57f, 7.654f, 9.986f};
-        System.out.println(Arrays.toString(three2));
-        System.out.println();
-        char[] three3 = {'o', 'n', 'e'};
-        System.out.println(Arrays.toString(three3));
-        System.out.println();
+        int[] expenses = {15000, 1500, 20000, 2500, 15000};
+        int sum = 0;
+        for (int i:expenses){
+           sum += i;
+        }
+        System.out.println("Сумма трат за месяц составила "+sum+" рублей\n");
 
         System.out.println("task 2"); // task 2 ****************************************
-        int i =0;
-        while (i < three.length - 1) {
-            System.out.print(three[i] + ", ");
-            i++;
+        int min = expenses[0], max = 0;
+        for (int i : expenses) {
+            if (min > i) min = i;
+            if (max < i) max = i;
         }
-        System.out.println(three[i]);
-        i = 0;
-        while (i < three2.length - 1) {
-            System.out.print(three2[i] + ", ");
-            i++;
-        }
-        System.out.println(three2[i]);
-        i = 0;
-        while (i < three3.length - 1) {
-            System.out.print(three3[i] + ", ");
-            i++;
-        }
-        System.out.println(three3[i]);
+        System.out.println("Минимальная сумма трат за неделю составила "+min+" рублей. Максимальная сумма трат за неделю составила "+max+" рублей");
         System.out.println();
 
         System.out.println("task 3"); // task 3 **********************************
-        i = three.length-1;
-        while (i > 0) {
-            System.out.print(three[i] + ", ");
-            i--;
+        float average = 0.0f;
+        sum = 0;
+        for (int i:expenses){
+            sum += i;
         }
-        System.out.println(three[i]);
-        i = three2.length-1;
-        while (i > 0) {
-            System.out.print(three2[i] + ", ");
-            i--;
-        }
-        System.out.println(three2[i]);
-        i = three3.length-1;
-        while (i > 0) {
-            System.out.print(three3[i] + ", ");
-            i--;
-        }
-        System.out.println(three3[i]);
-        System.out.println();
+        System.out.println("Средняя сумма трат за месяц составила "+(sum/(expenses.length))+" рублей\n");
 
         System.out.println("task 4"); // task 4 **********************************
-        i = 0;
-        while (i < three.length){
-            if (three[i]%2 == 0) three[i]++;
-            i++;
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        char c;
+        int b = reverseFullName.length-1;
+        for (int a = 0; a <= (reverseFullName.length / 2)-1; a ++){  // в массиве первые элементы меняются с последними, цикл работает до середины длины массива
+            c = reverseFullName[b];
+            reverseFullName[b] = reverseFullName[a];
+            reverseFullName[a] = c;
+            b --;
+
         }
-        System.out.println(Arrays.toString(three));
+        System.out.println(Arrays.toString(reverseFullName));
         System.out.println("End");
 
     }
